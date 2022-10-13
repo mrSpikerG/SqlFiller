28380416229d694a1d352e659f2f32ba62c748c3
thespiker <Fili_vw23@student.itstep.org>
2022-10-13 21:07:41 +0300

    add main functional

--- Program.cs
+++ Program.cs
@@ -1,15 +1,138 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using SQLFiller.model;
 
 namespace SQLFiller
 {
+
+    //--SQL 
+    //        --подбирать данные
+    //       --вывод sql и сохранение в файл
+
+    /*    CREATE TABLE BUYERS(
+    [BuyerCode] INT PRIMARY KEY,
+    [FIO] VARCHAR(100),
+    [Login] VARCHAR(32),
+    [Email] VARCHAR(100),
+    [Phone] VARCHAR(20),
+    [Address] VARCHAR(100)
+    );*/
+    //VALUES ('',N,)
     class Program
     {
+        public static List<SQLVariable> SQLVariables = new List<SQLVariable>();
+
+        public static Random random = new Random();
+
+
         static void Main(string[] args)
         {
+            if (!Directory.Exists("C:\\Users\\thespiker\\Documents\\sqlfiller"))
+            {
+                Directory.CreateDirectory("C:\\Users\\thespiker\\Documents\\sqlfiller");
+            }
+            string[] files = Directory.GetFiles("C:\\Users\\thespiker\\Documents\\sqlfiller");
+
+
+            foreach (var i in files)
+            {
+                if (i.EndsWith(".sql"))
+                {
+                    try
+                    {
+                        // bigint numeric bit smallint decimal smallmoney int tinyint money
+
+                        string text = File.ReadAllText(i);
+
+
+                        int startindex = text.IndexOf('(') + 1;
+                        text = text.Substring(startindex, text.LastIndexOf(')') - startindex);
+
+                        string[] variables = text.Split(',');
+                        for (int j = 0; j < variables.Length; j++)
+                        {
+
+                            string[] TEMP = variables[j].Split(' ');
+                            
+                            
+                            if(j== variables.Length - 1)
+                            {
+                                TEMP[1] = TEMP[1].Substring(0, TEMP[1].Length-2);
+                            }
+                            
+                            SQLVariables.Add(new SQLVariable(TEMP[1].ToUpper(), TEMP[0].Substring(3, TEMP[0].Length - 4)));
+                        }
+                    }
+                    catch (FileNotFoundException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        continue;
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < SQLVariables.Count; i++)
+            {
+                Console.WriteLine(SQLVariables[i]);
+            }
+
+
+                string sqlstring = "INSERT INTO TABLE(";
+            for (int i = 0; i < SQLVariables.Count; i++)
+            {
+                sqlstring += $"[{SQLVariables[i].name}]";
+                if (i != SQLVariables.Count - 1)
+                {
+                    sqlstring += ",";
+                }
+
+            }
+            sqlstring += ") VALUES";
+
+            int count = 10;
+            for (int j = 0; j < count; j++)
+            {
+                sqlstring += "\n(";
+                for (int i = 0; i < SQLVariables.Count; i++)
+                {
+
+                    sqlstring += $"'{SQLVariables[i].value}'";
+                    if (i != SQLVariables.Count - 1)
+                    {
+                        sqlstring += ", ";
+                    }
+                }
+                sqlstring += ")";
+                if (j != count - 1)
+                {
+                    sqlstring += ",";
+                }
+                else
+                {
+                    sqlstring += ";";
+                }
+
+                regenerateList();
+
+            }
+            Console.WriteLine(sqlstring);
         }
+
+
+
+        public static void regenerateList()
+        {
+            for (int i = 0; i < SQLVariables.Count; i++)
+            {
+                SQLVariables[i].generateValues();
+            }
+        }
+
     }
 }
---
+++ model/SQLVariable.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLFiller.model
+{
+    public class SQLVariable
+    {
+        public string type;
+        public string name;
+        public string value;
+
+        public Random random = new Random();
+        public SQLVariable(string type, string name)
+        {
+            this.type = type;
+            this.name = name;
+
+
+            generateValues();
+        }
+
+
+        public void generateValues()
+        {
+            switch (type)
+            {
+                case "SMALLMONEY":
+                case "INT":
+                    this.value = getRandomNumbers(int.MinValue, int.MaxValue);
+                    break;
+                case "MONEY":
+                case "BIGINT":
+                    this.value = LongRandom(long.MinValue, long.MaxValue).ToString();
+                    break;
+                case "SMALLINT":
+                    this.value = getRandomNumbers(short.MinValue, short.MaxValue);
+                    break;
+                case "TINYINT":
+                    this.value = getRandomNumbers(0, 255);
+                    break;
+                case "FLOAT":
+                    this.value = GetRandomFloat(double.MinValue, double.MaxValue);
+                    break;
+                case "REAL":
+                    this.value = GetRandomFloat(float.MinValue, float.MaxValue);
+                    break;
+                case "TEXT":
+                case "NTEXT":
+                    checkForName(int.MaxValue);
+                    break;
+            }
+        }
+
+        public void checkForName(int maxchars)
+        {
+            switch (name.ToLower())
+            {
+                case "phone":
+                    this.value = getPhoneNumber();
+                    return;
+                case "age":
+                    this.value = getAge();
+                    return;
+                case "email":
+                case "mail":
+                    this.value = getMail();
+                    return;
+                case "fio":
+                    this.value = getFIO();
+                    return;
+            }
+            this.value = "random";
+            return;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{type} {name} {value}";
+        }
+
+
+        public string generateRandomWord()
+        {
+            return "random";
+        }
+
+        //
+        //Номер
+        //
+        public string getPhoneNumber()
+        {
+
+            string[] value2 = new string[3];
+            string value3;
+            value3 = random.Next(100, 1000).ToString();
+            for (int i = 0; i < 3; i++)
+            {
+                value2[i] = random.Next(10, 100).ToString();
+            }
+
+            return $"+38(0{value2[0]}) {value3} {value2[1]} {value2[2]}";
+        }
+
+        //
+        //Возраст
+        //
+        public string getAge()
+        {
+            string age;
+            age = random.Next(4, 85).ToString();
+            return age;
+        }
+
+        //
+        //gmail
+        //
+        public string getMail()
+        {
+            int n = random.Next(100000);
+            string mail = $"newmail{n}@gmail.com";
+            return mail;
+        }
+
+        //
+        //randomNumbers
+        //
+        public string getRandomNumbers(int min, int max)
+        {
+            return random.Next(min, max).ToString();
+        }
+
+
+        public string GetRandomFloat(double minimum, double maximum)
+        {
+            return (random.NextDouble() * (maximum - minimum) + minimum).ToString();
+        }
+
+
+
+        //
+        //  random long
+        //
+        public long LongRandom(long min, long max)
+        {
+            long result = random.Next((Int32)(min >> 32), (Int32)(max >> 32));
+            result = (result << 32);
+            result = result | (long)random.Next((Int32)min, (Int32)max);
+            return result;
+        }
+
+        //
+        //FIO
+        //
+        public string getFIO()
+        {
+            string[] name = { "Alex", "Ivan", "Alica", "Ilya", "Viktoria", "Davud", "Diana", "Eva", "Nastya", "Yaroslav" };
+            string[] surname = { "Williams", "Peters", "Gibson", "Martin", "Jordan", "Jackson", "Grant", "Collins", "Bradley", "Barlow" };
+            int rName = random.Next(10);
+            int rSurname = random.Next(10);
+            return $"{name[rName]} {surname[rSurname]}";
+        }
+        //
+        //Name
+        //
+        public string getName()
+        {
+            string[] name = { "Alex", "Ivan", "Alica", "Ilya", "Viktoria", "Davud", "Diana", "Eva", "Nastya", "Yaroslav" };
+            int rName = random.Next(10);
+            return $"{name[rName]}";
+           
+        }
+    }
+}
